ef7d17a50632b4009dd55bf19f4f2c52a4f1f4c0
sudhansu-sekhar-panda <sudhansu1882@gmail.com>
2024-12-27 23:25:58 +0530

    task added

---
+++ class9/NonStaticVariable.java
@@ -0,0 +1,10 @@
+
+
+public class NonStaticVariable {
+    int x = 10;
+    public static void main(String [] args){
+        NonStaticVariable t1 = new NonStaticVariable();
+        System.out.println(t1.x);
+        
+    }
+}
--- class9/StaticVariable.java
+++ class9/StaticVariable.java
@@ -1,10 +1,9 @@
-
-
 public class StaticVariable {
-    int x = 10;
-    public static void main(String [] args){
+    static int x =10;
+    public static void main(String[] args) {
         StaticVariable t1 = new StaticVariable();
+        System.out.println(x);
         System.out.println(t1.x);
-        
+        System.out.println(StaticVariable.x);
     }
 }
