9eaed15842b06b83c9b0a628a6616af6191d510c
sudhansu-sekhar-panda <sudhansu1882@gmail.com>
2024-12-27 23:19:54 +0530

    task added

---
+++ class9/StaticVariable.java
@@ -0,0 +1,10 @@
+
+
+public class StaticVariable {
+    int x = 10;
+    public static void main(String [] args){
+        StaticVariable t1 = new StaticVariable();
+        System.out.println(t1.x);
+        
+    }
+}
