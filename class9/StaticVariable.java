b313654b0aebda3c828447bc3774563b95f8fb1e
sudhansu-sekhar-panda <sudhansu1882@gmail.com>
2025-01-05 19:10:22 +0530

    done

--- class9/StaticVariable.java
+++
@@ -1,9 +0,0 @@
-public class StaticVariable {
-    static int x =10;
-    public static void main(String[] args) {
-        StaticVariable t1 = new StaticVariable();
-        System.out.println(x);
-        System.out.println(t1.x);
-        System.out.println(StaticVariable.x);
-    }
-}
